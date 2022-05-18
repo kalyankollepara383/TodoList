7f2c3246416a2277aa3e30a2a426b4bd1e45c073
kalyankollepara383 <kalyankollepara@gmail.com>
2022-05-18 14:46:32 +0530

    Dynamic Content added

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import react,{useState} from 'react';
 
 function App() {
+  const[name,setName]=useState('Kalyan')
+  const changeHandler=()=>{
+    const username =name=="Kalyan" ? "Pavan" :"Kalyan"
+    setName(username)
+  }
   return (
     <div>
       <h4 className="bg-primary text-white text-center p-2">
-        To Do List
+       {name} To Do List
       </h4>
+      <button className='btn btn-primary text-center text-white, br-2 ml-2' onClick={changeHandler}>Change</button>
     </div>
   );
 }
